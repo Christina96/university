032f0060537fc2da3cc316bb56d4d56d8dcb874f
Christina <christinachaniotaki96@gmail.com>
2017-10-25 23:53:37 +0300

    -Fixes Fix a problem with the graph. Now the algorithm first sort the person by number of handshakes and then creates the algorithm. We are doing this because we want first to connect the biggest number of handshakes.

--- handshake_graph/handshake_graph.py
+++ handshake_graph/handshake_graph.py
@@ -2,6 +2,7 @@
 Handshakes Graph Construction Problem
 author: Christina Chaniotaki
 """
+import operator
 import random as rd
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -14,33 +15,53 @@ def handshakes_graph(handshakes):
     Melania Trump
     :return: a NetworkX graph with handshakes
     """
+    sorted_person_list = sort_people(handshakes)
     graph = nx.Graph()
     length = len(handshakes)
     # Add length + 1 nodes in the graph because we have Donald Trump too
     graph.add_nodes_from([i for i in range(0, length + 1)])
-    for person in range(0, length):
+    for person in sorted_person_list:
         for handshake in range(0, handshakes[person]):
+            if handshakes[person] == len(list(graph.adj[person])):
+                break
             check = True
             while check:
                 # a random person who had handshake with person
                 handshake_person = rd.randint(0, length)
-                if handshake_person == length:
+                if handshake_person == length and handshake_person not in list(
+                        graph.adj[person]) and handshake_person != person:
                     graph.add_edge(person, handshake_person)
                     check = False
-                elif handshake_person \
-                        not in list(graph.adj[person]) \
-                        and len(list(graph.adj[person])) < \
-                        handshakes[handshake_person]:
+                elif handshake_person not in list(graph.adj[person]) \
+                        and len(list(graph.adj[handshake_person])) < \
+                                handshakes[handshake_person] \
+                        and handshake_person != person:
                     graph.add_edge(person, handshake_person)
                     check = False
     return graph
 
 
+def sort_people(handshakes):
+    """
+    Sort the people by their handshakes
+    :param handshakes: list we the handshakes for each person
+    :return: sorted people by the number of handshakes
+    """
+    people_handshakes = {}
+    for i in range(0, len(handshakes)):
+        people_handshakes[i] = handshakes[i]
+    sorted_handshakes_person = sorted(people_handshakes.items(),
+                                      key=operator.itemgetter(1), reverse=True)
+    sorted_person_list = []
+    for i in sorted_handshakes_person:
+        sorted_person_list.append(i[0])
+    return sorted_person_list
+
+
 def main():
-    """ The beginning of the program that calls the functions."""
+    """" The beginning of the program that calls the functions."""
     handshakes = [i for i in range(0, 101)]
     graph = handshakes_graph(handshakes)
-    plt.figure(figsize=(16, 16))
     nx.draw_circular(graph, arrows=True, node_color='#A0CBE2', width=1,
                      with_labels=True)
     plt.show()
